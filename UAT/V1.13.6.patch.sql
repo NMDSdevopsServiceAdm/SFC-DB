923343f6457902a8f24f8f3bb675bf01e41b2070
Rishabh Chitranshi <rishabh.chitranshi@soprasteria.com>
2019-11-04 13:16:43 +0530

    Added NOT NULL

--- UAT/V1.13.6.patch.sql
+++ UAT/V1.13.6.patch.sql
@@ -1,4 +1,4 @@
 BEGIN TRANSACTION;
 -- Adding new column : CreatedByUserUID
-ALTER TABLE cqc."Notifications" ADD COLUMN "createdByUserUID" uuid;
+ALTER TABLE cqc."Notifications" ADD COLUMN "createdByUserUID" UUID NOT NULL;
 END TRANSACTION;
