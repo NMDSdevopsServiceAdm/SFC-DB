880a98b8ea4154b57eae6dcf6cd00f67c35549fb
Paul Humphreys <pjshumphreys@blueyonder.co.uk>
2019-11-15 18:17:37 +0000

    added bulk upload state column

--- UAT/V1.13.7.patch.sql
+++ UAT/V1.13.7.patch.sql
@@ -1,15 +1,15 @@
 BEGIN TRANSACTION;
 
-ALTER TABLE cqc."Establishment"
-    ADD COLUMN "bulkUploadLockHeld" boolean NOT NULL DEFAULT false;
-
-ALTER TABLE cqc."Establishment"
-    ADD COLUMN "bulkUploadState" cqc."bulkUploadStates" NOT NULL DEFAULT 'READY'::cqc."bulkUploadStates";
-
 CREATE TYPE cqc."bulkUploadStates" AS ENUM
     ('READY', 'DOWNLOADING', 'UPLOADING', 'UPLOADED', 'VALIDATING', 'FAILED', 'WARNINGS', 'PASSED', 'COMPLETING');
 
 ALTER TYPE cqc."bulkUploadStates"
     OWNER TO sfcadmin;
+
+ALTER TABLE cqc."Establishment"
+    ADD COLUMN "bulkUploadLockHeld" boolean NOT NULL DEFAULT false;
+
+ALTER TABLE cqc."Establishment"
+    ADD COLUMN "bulkUploadState" cqc."bulkUploadStates" NOT NULL DEFAULT 'READY'::cqc."bulkUploadStates";
 
 END TRANSACTION;
