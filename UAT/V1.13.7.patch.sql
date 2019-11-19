2bb89071f6e22905244fce2c97460dde69f2b9d9
Tikaram Choudhury <9570987+choudhuryt@users.noreply.github.com>
2019-11-19 14:39:05 +0530

    Update V1.13.7.patch.sql

--- UAT/V1.13.7.patch.sql
+++ UAT/V1.13.7.patch.sql
@@ -3,9 +3,6 @@ BEGIN TRANSACTION;
 CREATE TYPE cqc."bulkUploadStates" AS ENUM
     ('READY', 'DOWNLOADING', 'UPLOADING', 'UPLOADED', 'VALIDATING', 'FAILED', 'WARNINGS', 'PASSED', 'COMPLETING');
 
-ALTER TYPE cqc."bulkUploadStates"
-    OWNER TO sfcadmin;
-
 ALTER TABLE cqc."Establishment"
     ADD COLUMN "bulkUploadLockHeld" boolean NOT NULL DEFAULT false;
 
@@ -13,3 +10,13 @@ ALTER TABLE cqc."Establishment"
     ADD COLUMN "bulkUploadState" cqc."bulkUploadStates" NOT NULL DEFAULT 'READY'::cqc."bulkUploadStates";
 
 END TRANSACTION;
+
+\a \t
+SELECT '';
+SELECT CASE SUBSTRING(CURRENT_DATABASE(),1,3)
+          WHEN 'sfc' THEN
+             'ALTER TYPE cqc."bulkUploadStates" OWNER TO sfcadmin;'
+          ELSE NULL
+       END;
+SELECT '';
+\t \a
