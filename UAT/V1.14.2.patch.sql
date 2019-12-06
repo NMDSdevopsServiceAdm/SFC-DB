647670d920a03e77761315096968a92b77bbab39
Anurag Negi <55235060+Negi-Anurag@users.noreply.github.com>
2019-12-06 15:56:21 +0530

    ADDED NEW NOTIFICATION TYPE (#24)

---
+++ UAT/V1.14.2.patch.sql
@@ -0,0 +1,10 @@
+SET SEARCH_PATH TO cqc;
+BEGIN TRANSACTION;
+
+-- Adding new value to enumeration for the notification type
+
+ALTER TYPE "NotificationType" ADD VALUE 'LINKTOPARENTREQUEST'; 
+ALTER TYPE "NotificationType" ADD VALUE 'LINKTOPARENTAPPROVED'; 
+ALTER TYPE "NotificationType" ADD VALUE 'LINKTOPARENTREJECTED'; 
+
+END TRANSACTION;
