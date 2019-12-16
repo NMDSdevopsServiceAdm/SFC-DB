6c4c0f5bd914a0633dd46b1eb2355dd168b42203
Rishabh Chitranshi <rishabh.chitranshi@soprasteria.com>
2019-12-16 11:09:10 +0530

    Changed column and type names.

--- UAT/V1.14.3.patch.sql
+++ UAT/V1.14.3.patch.sql
@@ -4,10 +4,10 @@ CREATE TYPE cqc."wdfReportStates" AS ENUM
     ('READY', 'DOWNLOADING', 'FAILED', 'WARNINGS', 'COMPLETING');
 
 ALTER TABLE cqc."Establishment"
-    ADD COLUMN "wdfReportLockHeld" boolean NOT NULL DEFAULT false;
+    ADD COLUMN "WDFReportLockHeld" boolean NOT NULL DEFAULT false;
 
 ALTER TABLE cqc."Establishment"
-    ADD COLUMN "wdfReportState" cqc."wdfReportStates" NOT NULL DEFAULT 'READY'::cqc."wdfReportStates";
+    ADD COLUMN "WDFReportState" cqc."WDFReportStates" NOT NULL DEFAULT 'READY'::cqc."WDFReportStates";
 
 END TRANSACTION;
 
@@ -15,7 +15,7 @@ END TRANSACTION;
 SELECT '';
 SELECT CASE SUBSTRING(CURRENT_DATABASE(),1,3)
           WHEN 'sfc' THEN
-             'ALTER TYPE cqc."wdfReportStates" OWNER TO sfcadmin;'
+             'ALTER TYPE cqc."WDFReportStates" OWNER TO sfcadmin;'
           ELSE NULL
        END;
 SELECT '';
