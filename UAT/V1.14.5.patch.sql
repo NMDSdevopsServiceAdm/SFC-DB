485be88495ea3f10f3cc2d29cabb167fc1152edb
Rishabh Chitranshi <rishabh.chitranshi@soprasteria.com>
2019-12-30 16:18:05 +0530

    Removed cqc from tables names.

--- UAT/V1.14.5.patch.sql
+++ UAT/V1.14.5.patch.sql
@@ -4,11 +4,31 @@ BEGIN TRANSACTION;
 CREATE TYPE "TrainingReportStates" AS ENUM
     ('READY', 'DOWNLOADING', 'FAILED', 'WARNINGS', 'COMPLETING');
 
-ALTER TABLE cqc."Establishment"
+ALTER TABLE "Establishment"
     ADD COLUMN "TrainingReportLockHeld" boolean NOT NULL DEFAULT false;
 
 ALTER TABLE "Establishment"
-    ADD COLUMN "TrainingReportState" cqc."TrainingReportStates" NOT NULL DEFAULT 'READY'::"TrainingReportStates";
+    ADD COLUMN "TrainingReportState" "TrainingReportStates" NOT NULL DEFAULT 'READY'::"TrainingReportStates";
+
+SELECT t.table_schema, t.table_name, c.column_name
+FROM information_schema.tables t
+JOIN information_schema.columns c ON c.table_name = t.table_name AND c.table_schema = t.table_schema
+WHERE t.table_schema = 'cqc'
+AND t.table_name = 'Establishment'
+AND c.column_name = 'TrainingReportState'
+ORDER BY 1,2,3;
+
+SELECT n.nspname as enum_schema,
+t.typname as enum_name,
+e.enumtypid,
+STRING_AGG(e.enumsortorder || ') ' || e.enumlabel,' ' ORDER BY e.enumsortorder) enumsortorder_enumlabel
+FROM pg_type t
+JOIN pg_enum e ON t.oid = e.enumtypid
+JOIN pg_catalog.pg_namespace n ON n.oid = t.typnamespace
+WHERE n.nspname = 'cqc'
+AND t.typname = 'TrainingReportStates'
+GROUP BY 1,2,3
+ORDER BY 1,2,3;
 
 END TRANSACTION;
 
