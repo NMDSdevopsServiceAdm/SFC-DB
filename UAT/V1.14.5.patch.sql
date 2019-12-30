0a737f37f36a0f2f9a30d5068a6eb7f23a87a0b1
rchitranshi <53287302+rchitranshi@users.noreply.github.com>
2019-12-30 16:30:40 +0530

    Added columns required for polling in training report. (#28)
    * Revert "Columns required for polling in training report"
    
    This reverts commit ab9fb932901ee9ed721d425f7c4d6f19d69ab510.
    
    * Added columns required for polling in training report.
    
    * Implemented feedback changes.
    
    * Removed cqc from tables names.

--- UAT/V1.14.5.patch.sql
+++ UAT/V1.14.5.patch.sql
@@ -1,13 +1,34 @@
+SET SEARCH_PATH TO cqc;
 BEGIN TRANSACTION;
 
-CREATE TYPE cqc."TrainingReportStates" AS ENUM
+CREATE TYPE "TrainingReportStates" AS ENUM
     ('READY', 'DOWNLOADING', 'FAILED', 'WARNINGS', 'COMPLETING');
 
-ALTER TABLE cqc."Establishment"
+ALTER TABLE "Establishment"
     ADD COLUMN "TrainingReportLockHeld" boolean NOT NULL DEFAULT false;
 
-ALTER TABLE cqc."Establishment"
-    ADD COLUMN "TrainingReportState" cqc."TrainingReportStates" NOT NULL DEFAULT 'READY'::cqc."TrainingReportStates";
+ALTER TABLE "Establishment"
+    ADD COLUMN "TrainingReportState" "TrainingReportStates" NOT NULL DEFAULT 'READY'::"TrainingReportStates";
+
+SELECT t.table_schema, t.table_name, c.column_name
+FROM information_schema.tables t
+JOIN information_schema.columns c ON c.table_name = t.table_name AND c.table_schema = t.table_schema
+WHERE t.table_schema = 'cqc'
+AND t.table_name = 'Establishment'
+AND c.column_name = 'TrainingReportState'
+ORDER BY 1,2,3;
+
+SELECT n.nspname as enum_schema,
+t.typname as enum_name,
+e.enumtypid,
+STRING_AGG(e.enumsortorder || ') ' || e.enumlabel,' ' ORDER BY e.enumsortorder) enumsortorder_enumlabel
+FROM pg_type t
+JOIN pg_enum e ON t.oid = e.enumtypid
+JOIN pg_catalog.pg_namespace n ON n.oid = t.typnamespace
+WHERE n.nspname = 'cqc'
+AND t.typname = 'TrainingReportStates'
+GROUP BY 1,2,3
+ORDER BY 1,2,3;
 
 END TRANSACTION;
 
@@ -15,7 +36,7 @@ END TRANSACTION;
 SELECT '';
 SELECT CASE SUBSTRING(CURRENT_DATABASE(),1,3)
           WHEN 'sfc' THEN
-             'ALTER TYPE cqc."TrainingReportStates" OWNER TO sfcadmin;'
+             'ALTER TYPE "TrainingReportStates" OWNER TO sfcadmin;'
           ELSE NULL
        END;
 SELECT '';
