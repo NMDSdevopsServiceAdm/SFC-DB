c1beed556af36f8461a15ad1f969e9a13ebfc911
Rishabh Chitranshi <rishabh.chitranshi@soprasteria.com>
2019-12-30 15:57:19 +0530

    Implemented feedback changes.

--- UAT/V1.14.5.patch.sql
+++ UAT/V1.14.5.patch.sql
@@ -1,13 +1,14 @@
+SET SEARCH_PATH TO cqc;
 BEGIN TRANSACTION;
 
-CREATE TYPE cqc."TrainingReportStates" AS ENUM
+CREATE TYPE "TrainingReportStates" AS ENUM
     ('READY', 'DOWNLOADING', 'FAILED', 'WARNINGS', 'COMPLETING');
 
 ALTER TABLE cqc."Establishment"
     ADD COLUMN "TrainingReportLockHeld" boolean NOT NULL DEFAULT false;
 
-ALTER TABLE cqc."Establishment"
-    ADD COLUMN "TrainingReportState" cqc."TrainingReportStates" NOT NULL DEFAULT 'READY'::cqc."TrainingReportStates";
+ALTER TABLE "Establishment"
+    ADD COLUMN "TrainingReportState" cqc."TrainingReportStates" NOT NULL DEFAULT 'READY'::"TrainingReportStates";
 
 END TRANSACTION;
 
@@ -15,7 +16,7 @@ END TRANSACTION;
 SELECT '';
 SELECT CASE SUBSTRING(CURRENT_DATABASE(),1,3)
           WHEN 'sfc' THEN
-             'ALTER TYPE cqc."TrainingReportStates" OWNER TO sfcadmin;'
+             'ALTER TYPE "TrainingReportStates" OWNER TO sfcadmin;'
           ELSE NULL
        END;
 SELECT '';
