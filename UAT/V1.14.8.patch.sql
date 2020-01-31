6a2a32c63d2849d4554c666f01d565a737822b65
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2020-01-31 08:39:17 +0000

    Changes for PE, added SELECT before and after to ensure changes have worked

--- UAT/V1.14.8.patch.sql
+++ UAT/V1.14.8.patch.sql
@@ -1,7 +1,11 @@
 BEGIN TRANSACTION;
 
+SELECT id,name,iscqcregistered FROM cqc.services WHERE id = 16;
+
 UPDATE cqc.services SET
 iscqcregistered = NULL::boolean WHERE
 id = 16;
+
+SELECT id,name,iscqcregistered FROM cqc.services WHERE id = 16;
 
 END TRANSACTION;
