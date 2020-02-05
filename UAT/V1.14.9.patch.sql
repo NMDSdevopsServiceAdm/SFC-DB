8e85e07ded2b8c60f5c310a0c654f0442ae6efe1
rchitranshi <53287302+rchitranshi@users.noreply.github.com>
2020-02-05 16:54:01 +0530

    Added permission check for MandatoryTraining_ID_seq (#36)

--- UAT/V1.14.9.patch.sql
+++ UAT/V1.14.9.patch.sql
@@ -4,36 +4,47 @@ SET SEARCH_PATH TO cqc;
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS "MandatoryTraining";
-CREATE TABLE "MandatoryTraining" (
-	"ID"                       SERIAL NOT NULL PRIMARY KEY,
-	"EstablishmentFK"          INTEGER NOT NULL,
-	"TrainingCategoryFK"       INTEGER NOT NULL,
-	"JobFK"                    INTEGER NOT NULL,
-	created                    TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
-	updated                    TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
-	"CreatedByUserUID"         UUID NOT NULL,
-	"UpdatedByUserUID"         UUID NOT NULL,
-	CONSTRAINT establishment_mandatory_training_fk FOREIGN KEY ("EstablishmentFK")
+CREATE TABLE "MandatoryTraining"
+(
+   "ID"                 SERIAL NOT NULL PRIMARY KEY,
+   "EstablishmentFK"    INTEGER NOT NULL,
+   "TrainingCategoryFK" INTEGER NOT NULL,
+   "JobFK"              INTEGER NOT NULL,
+   created              TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+   updated              TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+   "CreatedByUserUID"   UUID NOT NULL,
+   "UpdatedByUserUID"   UUID NOT NULL,
+   CONSTRAINT establishment_mandatory_training_fk FOREIGN KEY ("EstablishmentFK")
       REFERENCES "Establishment" ("EstablishmentID") MATCH SIMPLE
       ON UPDATE NO ACTION
       ON DELETE NO ACTION,
-    CONSTRAINT worker_training_mandatory_training_category_fk FOREIGN KEY ("TrainingCategoryFK")
+   CONSTRAINT worker_training_mandatory_training_category_fk FOREIGN KEY ("TrainingCategoryFK")
       REFERENCES "TrainingCategories" ("ID") MATCH SIMPLE
       ON UPDATE NO ACTION
       ON DELETE NO ACTION,  
-    CONSTRAINT job_mandatory_training_fk FOREIGN KEY ("JobFK")
+   CONSTRAINT job_mandatory_training_fk FOREIGN KEY ("JobFK")
       REFERENCES "Job" ("JobID") MATCH SIMPLE
       ON UPDATE NO ACTION
       ON DELETE NO ACTION,  
-  	CONSTRAINT user_mandatory_training_created_by_fk FOREIGN KEY ("CreatedByUserUID")
+   CONSTRAINT user_mandatory_training_created_by_fk FOREIGN KEY ("CreatedByUserUID")
       REFERENCES "User" ("UserUID") MATCH SIMPLE
       ON UPDATE NO ACTION
       ON DELETE NO ACTION,
-  	CONSTRAINT user_owner_change_request_updated_by_fk FOREIGN KEY ("UpdatedByUserUID")
+   CONSTRAINT user_owner_change_request_updated_by_fk FOREIGN KEY ("UpdatedByUserUID")
       REFERENCES "User" ("UserUID") MATCH SIMPLE
       ON UPDATE NO ACTION
       ON DELETE NO ACTION
 );
+
+CREATE SEQUENCE IF NOT EXISTS "MandatoryTraining_ID_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER SEQUENCE IF EXISTS "MandatoryTraining_ID_seq" OWNED BY "MandatoryTraining"."ID";
 
 END TRANSACTION;
 
@@ -47,7 +58,9 @@ SELECT CASE SUBSTRING(CURRENT_DATABASE(),1,3)
              'GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE "MandatoryTraining" TO "Sfc_Admin_Role";' || E'\n' ||
              'GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE "MandatoryTraining" TO "Sfc_App_Role";' || E'\n' ||
              'GRANT INSERT, SELECT, UPDATE ON TABLE "MandatoryTraining" TO "Read_Update_Role";' || E'\n' ||
-             'GRANT SELECT ON TABLE "MandatoryTraining" TO "Read_Only_Role";'
+             'GRANT SELECT ON TABLE "MandatoryTraining" TO "Read_Only_Role";' || E'\n' ||
+             'GRANT USAGE, SELECT ON SEQUENCE "MandatoryTraining_ID_seq" TO sfcadmin;' || E'\n' ||
+             'GRANT USAGE, SELECT ON SEQUENCE "MandatoryTraining_ID_seq" TO "Sfc_App_Role";'
           ELSE NULL
        END;
 SELECT '';
