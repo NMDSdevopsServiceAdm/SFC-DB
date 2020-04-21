64779108b39ef2a3a8bc2ef19d6f1341f7b8b45e
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-04-21 15:07:27 +0100

    Added a GROUP BY and used partial postcode matching

--- ad-hoc-scripts/workplace-ids-starting-w.sql
+++ ad-hoc-scripts/workplace-ids-starting-w.sql
@@ -2,16 +2,17 @@ SELECT
 	estab."NmdsID",
 	cssr."NmdsIDLetter" as prefix,
 	concat(cssr."NmdsIDLetter", trim(leading 'W' from estab."NmdsID")) as newWorkplaceId,
-	UPPER(estab."PostCode") as postcode,
+    UPPER(estab."PostCode") as postcode,
 	pcode."local_custodian_code",
 	cssr."LocalAuthority"
 FROM 
 	cqc."Establishment" estab 
 LEFT OUTER JOIN
 	cqcref."pcode" pcode 
-		ON UPPER(pcode."postcode") 
-			= UPPER(estab."PostCode")
+		ON UPPER(substring(pcode."postcode" from '[^ ]+'::text))
+			= UPPER(substring(estab."PostCode" from '[^ ]+'::text))
 LEFT OUTER JOIN
 	cqc."Cssr" cssr ON cssr."LocalCustodianCode" = pcode."local_custodian_code"
 WHERE 
 	estab."NmdsID" LIKE 'W%'
+	GROUP BY estab."NmdsID", cssr."NmdsIDLetter"
