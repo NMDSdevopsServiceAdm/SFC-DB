fb761f3953960cb254f230bfba6b5180c32b7f96
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-04-22 14:28:18 +0100

    Speed things up by trimming and using = instead of LIKE

--- ad-hoc-scripts/workplace-ids-starting-w.sql
+++ ad-hoc-scripts/workplace-ids-starting-w.sql
@@ -5,7 +5,7 @@ SELECT
             ELSE STRING_AGG(DISTINCT concat(cssr."NmdsIDLetter", trim(leading 'W' from estab."NmdsID")), ',')
     END AS newWorkplaceId,
 	estab."PostCode",
-    REVERSE(substring(REVERSE(estab."PostCode") from 4)) AS MatchedPcodePrefix,
+    trim(trailing from REVERSE(substring(REVERSE(UPPER(estab."PostCode")) from 4))) AS MatchedPcodePrefix,
 	estab."NameValue",
     STRING_AGG(DISTINCT cssr."LocalCustodianCode"::text, ',') AS CustodianCodes,
     STRING_AGG(DISTINCT cssr."LocalAuthority", ',') AS LAs
@@ -13,12 +13,28 @@ FROM
 	cqc."Establishment" estab 
 LEFT OUTER JOIN
 	cqcref."pcode" pcode 
-		ON UPPER(pcode."postcode")
+		ON trim(trailing from 
+                REVERSE(
+                    substring(
+                        REVERSE(
+                            UPPER(pcode."postcode")
+                        ) 
+                    from 4)
+                )
+            )
             /* Some postcodes in the establishment table don't have spaces.
              What we want is everything up to the last 3 chars.
              We're reversing, removing the first 3 chars, reversing again, 
              and then adding '%' to use LIKE to find postcodes that start with the result */
-            LIKE concat(REVERSE(substring(REVERSE(UPPER(estab."PostCode")) from 4)),'%')
+            = trim(trailing from 
+                REVERSE(
+                    substring(
+                        REVERSE(
+                            UPPER(estab."PostCode")
+                        ) 
+                    from 4)
+                )
+            )
 LEFT OUTER JOIN
 	cqc."Cssr" cssr ON cssr."LocalCustodianCode" = pcode."local_custodian_code"
 WHERE 
