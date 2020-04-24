913fd148fe7a9a14c32cde12dadceeda227c68cc
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-04-24 18:22:59 +0100

    Added useful select statement

--- Update-scripts/workplaceid-w/staging-update-workplace-ids-starting-w.sql
+++ Update-scripts/workplaceid-w/staging-update-workplace-ids-starting-w.sql
@@ -5,3 +5,8 @@ UPDATE cqc."Establishment" SET "NmdsID" = 'I1001419' WHERE  "NmdsID" = 'W1001419
 UPDATE cqc."Establishment" SET "NmdsID" = 'J1001494' WHERE  "NmdsID" = 'W1001494';
 
 COMMIT;
+
+-- select estab."NmdsID", estab."NameValue" from cqc."Establishment" estab where estab."NmdsID" 
+-- IN ('W1001417','W1001419','W1001494');
+-- select estab."NameValue" from cqc."Establishment" estab where estab."NmdsID" = 'W1001494';
+
