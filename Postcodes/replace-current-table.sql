06bed96e8abd7579c171a24cc6e07c17abb6ee30
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-07-17 10:14:22 +0100

    Add explanatory comments to new scripts

--- Postcodes/replace-current-table.sql
+++ Postcodes/replace-current-table.sql
@@ -1,3 +1,6 @@
+----------------
+-- Use this at the end of importing new postcode data, to swap the new table in and backup the old one.
+----------------
 BEGIN TRANSACTION;
 ----------------
 ALTER TABLE cqcref."pcodedata" RENAME TO "pcodedata-backup";
--- Postcodes/revert-current-table.sql
+++ Postcodes/revert-current-table.sql
@@ -1,3 +1,6 @@
+----------------
+-- Use this if there is a problem with new postcode data and you want to bring the old postcode data back.
+----------------
 BEGIN TRANSACTION;
 ----------------
 ALTER TABLE cqcref."pcodedata" RENAME TO "pcodedata-backup2";
