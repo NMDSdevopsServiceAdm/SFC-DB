9ad95fa68e116ca3369c65db0ae55967febd7c39
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-07-22 15:29:31 +0100

    bugfix/anbalysis-blanks: experimenting with getting correct results.

--- Analysis-file/analysis_file_report_blank-investigation.sql
+++ Analysis-file/analysis_file_report_blank-investigation.sql
@@ -1,10 +1,11 @@
+SET SEARCH_PATH TO cqc;
 ------------------------------
 SELECT 
-       "NumberOfStaffValue" totalstaff, --****COALESCE**** -- 038
+       COALESCE("NumberOfStaffValue", -1) totalstaff, --****COALESCE**** -- 038
        CASE 
-          WHEN "StartersValue" = 'None' THEN 0
+          WHEN "StartersValue" = 'None' THEN 0 -- happens when you actively select "None"
           WHEN "StartersValue" = 'Don''t know' THEN -2
-          WHEN "StartersValue" IS NULL THEN -1
+          WHEN "StartersValue" IS NULL THEN -1 -- happens when you have brand new workplace and nothing entered
           ELSE (SELECT SUM("Total") FROM "EstablishmentJobs" WHERE "EstablishmentID" = e."EstablishmentID" AND "JobType" = 'Starters')
        END totalstarters, --****COALESCE**** -- 044
        CASE
