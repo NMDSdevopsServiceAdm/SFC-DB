2304e533fda5b78c2af6b7df6f51c7914e162818
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2020-07-29 13:31:18 +0100

    Added script to import benchmark CSV

---
+++ Benchmarks/import.sql
@@ -0,0 +1,57 @@
+BEGIN TRANSACTION;
+
+-------------------
+-- Turn timing on so we can see how long it took
+-------------------
+\timing
+
+-------------------
+-- Create a temp table, "benchmarks-source", to hold the new postcode data from the csv
+-------------------
+SELECT 'Creating temp source table';
+
+DROP TABLE IF EXISTS cqc."Benchmarks_new";
+
+CREATE TABLE cqc."Benchmarks_new"
+(
+    "CssrIDFK" integer NOT NULL,
+    "MainServiceFK" integer NOT NULL,
+    "Pay" integer,
+    "Sickness" integer,
+    "Turnover" numeric(3,2),
+    "Qualifications" numeric(3,2),
+    "Workplaces" integer NOT NULL,
+    "Staff" integer NOT NULL,
+    CONSTRAINT "Benchmarks_MainServiceFK_fkey" FOREIGN KEY ("MainServiceFK")
+        REFERENCES cqc.services ("reportingID") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+ALTER TABLE cqc."Benchmarks_new"
+    OWNER to sfcadmin;
+
+-------------------
+-- Import the new benchmark data from the csvs into your temp table
+-- !! Edit the path to the csvs !!
+-------------------
+SELECT 'Importing new benchmark data from the csvs into temp source table';
+TRUNCATE cqc."Benchmarks_new";
+\copy cqc."Benchmarks_new" FROM 'C:\Users\arussell\Downloads\File_format_for_comparisons.csv' WITH (FORMAT csv);
+
+-------------------
+-- Check new data successfully updated
+-------------------
+SELECT 
+  *
+FROM 
+  cqc."Benchmarks_new" 
+WHERE 
+  "CssrIDFK" = '1';
+
+ALTER TABLE cqc."Benchmarks" RENAME TO "Benchmarks-backup";
+----------------
+ALTER TABLE cqc."Benchmarks_new" RENAME TO "Benchmarks";
+
+----------------
+END TRANSACTION;
