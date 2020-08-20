118c784224758fab86423ac136a03fe42c2e86df
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-08-20 17:45:54 +0100

    feat/postcodes: Remove commented-out code.

--- Postcodes/return-new-table.sql
+++ Postcodes/return-new-table.sql
@@ -8,10 +8,3 @@ ALTER TABLE cqcref."pcodedata" RENAME TO "pcodedata-backup";
 ALTER TABLE cqcref."pcodedata-backup2" RENAME TO "pcodedata";
 ----------------
 END TRANSACTION;
-
--- select "NameOrIdValue" from cqc."Worker" where "NameOrIdValue" like 'Aaron in%' limit 10;
--- select "postcode" from cqcref."pcodedata" limit 1;
--- select "postcode" from cqcref."pcodedata-backup" limit 1;
--- select "postcode" from cqcref."pcodedata-backup2" limit 1;
--- select "uprn", "postcode" from cqcref."pcodedata-backup" where "postcode" = 'NW9 4EW';
--- select "uprn", "postcode" from cqcref."pcodedata" where "postcode" = 'NW9 4EW';
