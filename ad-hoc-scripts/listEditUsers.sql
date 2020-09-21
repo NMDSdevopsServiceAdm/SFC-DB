9cfaf89aa26a1066418d9542c2ff9977e47290b9
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2020-09-21 14:30:29 +0100

    Added SQL to get edit users from standalone and data owners

---
+++ ad-hoc-scripts/listEditUsers.sql
@@ -0,0 +1,22 @@
+SELECT 
+	est."NmdsID", 
+	est."NameValue", 
+	usr."FullNameValue",
+    CASE
+		WHEN usr."IsPrimary" = true THEN 'Yes'
+		ELSE 'No'
+	END as primaryuser,
+    usr."EmailValue"
+FROM cqc."Establishment" as est
+FULL OUTER JOIN cqc."User" as usr ON est."EstablishmentID" = usr."EstablishmentID"
+WHERE
+    est."DataOwner" = 'Workplace'
+AND
+	est."IsParent" = false
+AND
+	est."Archived" = false
+AND
+	usr."Archived" = false
+AND
+	usr."UserRoleValue" = 'Edit'
+;
