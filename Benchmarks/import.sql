a8e1d3e05feed436175092d107625f319a3599e3
Aaron Russell <aaron.russell@skillsforcare.org.uk>
2021-01-21 12:30:10 +0000

    Changed to relative paths and added files folder

--- Benchmarks/import.sql
+++ Benchmarks/import.sql
@@ -7,11 +7,11 @@ BEGIN TRANSACTION;
 
 TRUNCATE cqc."Benchmarks", cqc."BenchmarksPay", cqc."BenchmarksTurnover", cqc."BenchmarksQualifications", cqc."BenchmarksSickness";
 
-\copy cqc."Benchmarks" ("CssrID", "MainServiceFK", "Pay", "Sickness", "Turnover", "Qualifications", "Workplaces", "Staff", "PayWorkplaces", "PayStaff", "SicknessWorkplaces", "SicknessStaff", "QualificationsWorkplaces", "QualificationsStaff", "TurnoverWorkplaces", "TurnoverStaff", "PayGoodCQC", "PayLowTurnover", "SicknessGoodCQC", "SicknessLowTurnover", "QualificationsGoodCQC", "QualificationsLowTurnover", "TurnoverGoodCQC", "TurnoverLowTurnover") FROM '/efs/benchmarks/Benchmarks.csv' WITH (FORMAT csv, ENCODING 'UTF8', HEADER);
-\copy cqc."BenchmarksPay" ("CssrID", "MainServiceFK", "EstablishmentFK", "Pay") FROM '/efs/benchmarks/BenchmarksPay.csv' WITH (FORMAT csv, ENCODING 'UTF8', HEADER);
-\copy cqc."BenchmarksTurnover" ("CssrID", "MainServiceFK", "EstablishmentFK", "Turnover") FROM '/efs/benchmarks/BenchmarksTurnover.csv' WITH (FORMAT csv, ENCODING 'UTF8', HEADER);
-\copy cqc."BenchmarksQualifications" ("CssrID", "MainServiceFK", "EstablishmentFK", "Qualifications") FROM '/efs/benchmarks/BenchmarksQuals.csv' WITH (FORMAT csv, ENCODING 'UTF8', HEADER);
-\copy cqc."BenchmarksSickness" ("CssrID", "MainServiceFK", "EstablishmentFK", "Sickness") FROM '/efs/benchmarks/BenchmarksSickness.csv' WITH (FORMAT csv, ENCODING 'UTF8', HEADER);
+\copy cqc."Benchmarks" ("CssrID", "MainServiceFK", "Pay", "Sickness", "Turnover", "Qualifications", "Workplaces", "Staff", "PayWorkplaces", "PayStaff", "SicknessWorkplaces", "SicknessStaff", "QualificationsWorkplaces", "QualificationsStaff", "TurnoverWorkplaces", "TurnoverStaff", "PayGoodCQC", "PayLowTurnover", "SicknessGoodCQC", "SicknessLowTurnover", "QualificationsGoodCQC", "QualificationsLowTurnover", "TurnoverGoodCQC", "TurnoverLowTurnover") FROM 'files/Benchmarks.csv' WITH (FORMAT csv, ENCODING 'UTF8', HEADER);
+\copy cqc."BenchmarksPay" ("CssrID", "MainServiceFK", "EstablishmentFK", "Pay") FROM 'files/BenchmarksPay.csv' WITH (FORMAT csv, ENCODING 'UTF8', HEADER);
+\copy cqc."BenchmarksTurnover" ("CssrID", "MainServiceFK", "EstablishmentFK", "Turnover") FROM 'files/BenchmarksTurnover.csv' WITH (FORMAT csv, ENCODING 'UTF8', HEADER);
+\copy cqc."BenchmarksQualifications" ("CssrID", "MainServiceFK", "EstablishmentFK", "Qualifications") FROM 'files/BenchmarksQualifications.csv' WITH (FORMAT csv, ENCODING 'UTF8', HEADER);
+\copy cqc."BenchmarksSickness" ("CssrID", "MainServiceFK", "EstablishmentFK", "Sickness") FROM 'files/BenchmarksSickness.csv' WITH (FORMAT csv, ENCODING 'UTF8', HEADER);
 
 INSERT INTO cqc."DataImports" ("Type", "Date") VALUES ('Benchmarks', current_timestamp);
 
