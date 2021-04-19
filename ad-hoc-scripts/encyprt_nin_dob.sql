8f5b44f0029cc29102f030f70a3b8d88b4be1c44
George <george.taylor@madetech.com>
2021-04-19 11:21:44 +0100

    Armoring the encrypted value

--- ad-hoc-scripts/encyprt_nin_dob.sql
+++ ad-hoc-scripts/encyprt_nin_dob.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 --NOTE: YOU NEED TO EXCHANGE ::PUBLICKEY WITH BASE64 ENCODED PUBLIC KEY
 update cqc."Worker"
-set "DateOfBirthEncryptedValue" = pgp_pub_encrypt("DateOfBirthValue" :: text, dearmor(convert_from(decode('::PUBLICKEY','base64'),'UTF8')));
+set "DateOfBirthEncryptedValue" = armor(pgp_pub_encrypt("DateOfBirthValue" :: text, dearmor(convert_from(decode('::PUBLICKEY','base64'),'UTF8'))));
 
 update cqc."Worker"
-set "NationalInsuranceNumberEncryptedValue" = pgp_pub_encrypt("NationalInsuranceNumberValue" :: text, dearmor(convert_from(decode('::PUBLIC KEY','base64'),'UTF8')));
+set "NationalInsuranceNumberEncryptedValue" = armor(pgp_pub_encrypt("NationalInsuranceNumberValue" :: text, dearmor(convert_from(decode('::PUBLIC KEY','base64'),'UTF8'))));
